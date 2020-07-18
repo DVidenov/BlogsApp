d6e77619ae0c1f28f3b11bf217d759309eb94256
Denislav Videnov <denislavv@helecloud.com>
2020-07-18 19:21:38 +0300

    adding the app so far, apis, html

--- src/app.py
+++ src/app.py
@@ -1,12 +1,67 @@
+from flask import Flask, render_template, request, session
 from flask import Flask
-
-from flask import Flask
+from src.common.database import Database
+from src.models.user import User
 
 app = Flask(__name__)
+app.secret_key = "denislav"
 
-@app.route('/') #www.mysite.com/api
-def hello_method():
-    return "Hello, world!"
+
+@app.route('/')
+def home_template():
+    return render_template('home.html')
+
+
+@app.route('/login') #www.mysite.com/api/login
+def login_template():
+    return render_template('login.html')
+
+
+@app.route('/register') #www.mysite.com/api/register
+def register_template():
+    return render_template('register.html')
+
+
+@app.before_first_request
+def initialize_database():
+    Database.initialize()
+
+
+@app.route('/auth/login', methods=['POST'])
+def login_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    if User.login_valid(email, password):
+        User.login(email)
+    else:
+        session['email'] = None
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/auth/register', methods=['POST'])
+def register_user():
+    email = request.form['email']
+    password = request.form['password']
+
+    User.register(email, password)
+
+    return render_template("profile.html", email=session['email'])
+
+
+@app.route('/blogs/<string:user_id>')
+@app.route('/blogs')
+def user_blogs(user_id=None):
+    if user_id is not None:
+        user = User.get_by_id(user_id)
+    else:
+        user = User.get_by_email(session['email'])
+
+    blogs = user.get_blogs()
+
+    return render_template("user_blogs.html", blogs=blogs, email=user.email)
+
 
 if __name__ == '__main__':
     app.run(port=4995)
